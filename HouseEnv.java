cd3cbd3b85f23e99b61d17fc4489eb9cfcac3fbb
PabloG02 <tioo23000@gmail.com>
2023-03-08 14:17:59 +0100

    Work done up to `Week 4 - Exercise 3`

---
+++ HouseEnv.java
@@ -0,0 +1,130 @@
+import jason.asSyntax.*;
+import jason.environment.Environment;
+import jason.environment.grid.Location;
+import java.util.logging.Logger;
+
+public class HouseEnv extends Environment {
+
+    // common literals
+    public static final Literal of  = Literal.parseLiteral("open(fridge)");
+    public static final Literal clf = Literal.parseLiteral("close(fridge)");
+    public static final Literal gb  = Literal.parseLiteral("get(beer)");
+    public static final Literal hb  = Literal.parseLiteral("hand_in(beer)");
+    public static final Literal sb  = Literal.parseLiteral("sip(beer)");
+    public static final Literal hob = Literal.parseLiteral("has(owner,beer)");
+
+    public static final Literal af = Literal.parseLiteral("at(robot,fridge)");
+    public static final Literal ao = Literal.parseLiteral("at(robot,owner)");
+    public static final Literal ap = Literal.parseLiteral("at(robot,pickup)");
+
+    static Logger logger = Logger.getLogger(HouseEnv.class.getName());
+
+    HouseModel model; // the model of the grid
+
+    @Override
+    public void init(String[] args) {
+        model = new HouseModel();
+
+        if (args.length == 1 && args[0].equals("gui")) {
+            HouseView view  = new HouseView(model);
+            model.setView(view);
+        }
+
+        updatePercepts();
+    }
+
+    /** creates the agents percepts based on the HouseModel */
+    void updatePercepts() {
+        // clear the percepts of the agents
+        clearPercepts("robot");
+        clearPercepts("owner");
+
+        // get the robot location
+        Location lRobot = model.getAgPos(0);
+
+        // add agent location to its percepts
+        if (lRobot.equals(model.nearFridge[0])) {
+            addPercept("robot", af);
+        }
+        if (lRobot.equals(model.nearOwner[0])) {
+            addPercept("robot", ao);
+        }
+        if (lRobot.equals(model.lPickUp)) {
+            addPercept("robot", ap);
+        }
+
+        // add beer "status" the percepts
+        if (model.fridgeOpen) {
+            addPercept("robot", Literal.parseLiteral("stock(beer,"+model.availableBeers+")"));
+        }
+		
+        if (model.sipCount > 0) {
+            addPercept("robot", hob);
+            addPercept("owner", hob);
+        }
+    }
+
+
+    @Override
+    public boolean executeAction(String ag, Structure action) {
+        System.out.println("["+ag+"] doing: "+action);
+        boolean result = false;
+        if (action.equals(of)) { // of = open(fridge)
+            result = model.openFridge();
+
+        } else if (action.equals(clf)) { // clf = close(fridge)
+            result = model.closeFridge();
+
+        } else if (action.getFunctor().equals("move_towards")) {
+            String l = action.getTerm(0).toString();
+            Location dest = null;
+            boolean adjacent = false;
+            if (l.equals("fridge")) {
+                dest = model.lFridge;
+                adjacent = true;
+            } else if (l.equals("owner")) {
+                dest = model.lOwner;
+                adjacent = true;
+            } else if (l.equals("pickup")) {
+                dest = model.lPickUp;
+            }
+
+            try {
+                result = adjacent? model.moveTowardsAdjacent(dest) : model.moveTowards(dest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else if (action.equals(gb) && ( ag.equals("robot") || ag.equals("robot") )) {
+            result = model.getBeer();
+
+        } else if (action.equals(hb) && ag.equals("robot")) {
+            result = model.handInBeer();
+
+        } else if (action.equals(sb) && ag.equals("owner")) {
+            result = model.sipBeer();
+
+        } else if (action.getFunctor().equals("deliver") && ag.equals("supermarket")) {
+            // wait 4 seconds to finish "deliver"
+            System.out.println("Acción:  " + action);
+            try {
+                Thread.sleep(4000);
+                model.deliveredBeers = (int)((NumberTerm)action.getTerm(1)).solve();
+                result = true;
+            } catch (Exception e) {
+                logger.info("Failed to execute action deliver!"+e);
+            }
+
+        } else {
+            logger.info("Failed to execute action "+action);
+        }
+
+        if (result) {
+            updatePercepts();
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {}
+        }
+        return result;
+    }
+}
---
+++ HouseView.java
@@ -0,0 +1,68 @@
+import jason.environment.grid.*;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+
+/** class that implements the View of Domestic Robot application */
+public class HouseView extends GridWorldView {
+
+    HouseModel hmodel;
+
+    public HouseView(HouseModel model) {
+        super(model, "Domestic Robot", 700);
+        hmodel = model;
+        defaultFont = new Font("Arial", Font.BOLD, 16); // change default font
+        setVisible(true);
+        repaint();
+    }
+
+    /** draw application objects */
+    @Override
+    public void draw(Graphics g, int x, int y, int object) {
+        Location lRobot = hmodel.getAgPos(0);
+        super.drawAgent(g, x, y, Color.lightGray, -1);
+        switch (object) {
+        case HouseModel.FRIDGE:
+            if (lRobot.equals(hmodel.nearFridge[0])) {
+                super.drawAgent(g, x, y, Color.cyan, -1);
+            }
+            g.setColor(Color.black);
+            drawString(g, x, y, defaultFont, "Fridge ("+hmodel.availableBeers+")");
+            break;
+        case HouseModel.OWNER:
+            if (lRobot.equals(hmodel.nearOwner[0])) {
+                super.drawAgent(g, x, y, Color.pink, -1);
+            }
+            String o = "Owner";
+            if (hmodel.sipCount > 0) {
+                o +=  " ("+hmodel.sipCount+")";
+            }
+            g.setColor(Color.black);
+            drawString(g, x, y, defaultFont, o);
+            break;
+        case HouseModel.PICKUP:
+            if (lRobot.equals(hmodel.lPickUp)) {
+                super.drawAgent(g, x, y, Color.green, -1);
+            }
+            g.setColor(Color.black);
+            drawString(g, x, y, defaultFont, "Pick up");
+            break;
+        }
+        repaint();
+    }
+
+    @Override
+    public void drawAgent(Graphics g, int x, int y, Color c, int id) {
+        Location lRobot = hmodel.getAgPos(0);
+        if (!lRobot.equals(hmodel.lOwner) && !lRobot.equals(hmodel.lFridge)) {
+            c = Color.yellow;
+            if (hmodel.carryingBeer) c = Color.orange;
+            if (hmodel.deliveredBeers != 0) c = new Color(250, 125, 0);
+            super.drawAgent(g, x, y, c, -1);
+            g.setColor(Color.black);
+            super.drawString(g, x, y, defaultFont, "Robot");
+        }
+    }
+}
